109d35f0a2d20870f9134554be03e094f6c445a4
ben <ben@sixpearls.com>
2019-09-23 09:02:02 -0700

    more string fixes

--- sweepable.py
+++ sweepable.py
@@ -61,7 +61,7 @@ type_to_field = {
     int: peewee.IntegerField,
     float: peewee.FloatField,
     bool: peewee.BooleanField,
-    str: peewee.CharField(max_length=255, default='')
+    str: peewee.CharField,
 }
 
 try:
@@ -530,7 +530,8 @@ class sweeper(object):
             if (isinstance(getattr(self.model, arg).default, str) and 
             isinstance(val, str)):
                 arg_rows = 1
-            arg_rows = getattr(val, '__len__', lambda: 1)()
+            else:
+                arg_rows = getattr(val, '__len__', lambda: 1)()
             if arg_rows > 1 and arg_rows != num_rows:
                 if num_rows == 1:
                     num_rows = arg_rows
